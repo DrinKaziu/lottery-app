a5d62ee354f4c28d3a554bd94268066c633b1dc4
SnowSurfer <drinkaziu@gmail.com>
2019-04-17 20:43:42 -0400

    add generate random numbers functionality

--- src/Lottery.js
+++ src/Lottery.js
@@ -2,25 +2,30 @@ import React, { Component } from 'react';
 import Ball from './Ball';
 
 class Lottery extends Component {
+
+  static defaultProps = {
+    title: 'Lotto',
+    numBalls: 6,
+    maxNum: 40
+  }
+
   constructor(props) {
     super(props);
     this.state = {
-      title: 'Lotto',
-      nums: [],
-      maxNum: 40
+      nums: Array.from({length: this.props.numBalls}) //create an array of empty slots so we have something to show
     }
   }
 
-  generateNums = () => {
-    let randomNum = Math.floor(Math.random() * this.state.maxNum) + 1; 
-    this.setState({nums: [ ...this.state.nums, randomNum ]});
+  generateNums = () => { 
+    this.setState(curState => ({
+      nums: curState.nums.map(n => Math.floor(Math.random() * this.props.maxNum) + 1)
+    }));
   }
 
   render () {
-    console.log(this.state.nums)
     return (
       <div className="Lottery">
-        <h2>{this.state.title}</h2>
+        <h2>{this.props.title}</h2>
         <ul>
           {this.state.nums.map(num => <Ball number={num}/>)}
         </ul>
